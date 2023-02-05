29a2c2f7f7e57528cfa4b3efb28f5a54b465ad2c
joao.ramos <ramosjp1993@gmail.com>
2023-02-05 17:03:55 +0000

    Nick Chapsas - JSON support gets a major missing feature in .NET 7

---
+++ JsonChanges/Points.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace JsonChanges
+{
+    [JsonPolymorphic(UnknownDerivedTypeHandling = JsonUnknownDerivedTypeHandling.FailSerialization, TypeDiscriminatorPropertyName = "Type")]
+    [JsonDerivedType(typeof(Point3D), "point3d")]
+    [JsonDerivedType(typeof(Point2D), "point2d")]
+    public class Point2D
+    {
+        public double X { get; set; }
+
+        public double Y { get; set; }
+    }
+
+    public class Point3D : Point2D
+    {
+        public double Z { get; set; }
+    }
+}
---
+++ JsonChanges/Program.cs
@@ -0,0 +1,16 @@
+﻿//
+// This code is based on Nick Chapsas Video JSON support gets a major missing feature in .NET 7
+// https://www.youtube.com/watch?v=2zUQwVD7T_E&ab_channel=NickChapsas
+// This example shows how to work with polymorphism when serializing and deserializing in JSON
+//
+
+using JsonChanges;
+using System.Text.Json;
+
+Point2D point2d = new Point2D { X = 4, Y = 20 };
+string point2dAsText = JsonSerializer.Serialize(point2d);
+Point2D point2dAdaing = JsonSerializer.Deserialize<Point2D>(point2dAsText)!;
+
+Point2D point3d = new Point3D { X = 4, Y = 20, Z = 30 };
+string point3dAsText = JsonSerializer.Serialize(point3d);
+Point3D point3dAdaing = JsonSerializer.Deserialize<Point3D>(point3dAsText)!;
