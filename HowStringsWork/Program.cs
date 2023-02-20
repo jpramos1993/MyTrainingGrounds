f583f20f307ecdae3bfd2e1f9c324e2ec55c3527
joao.ramos <ramosjp1993@gmail.com>
2023-02-20 20:18:29 +0000

    Nick Chapsas - How Strings Work
    https://www.youtube.com/watch?v=Xeq8YGEyyp8&ab_channel=NickChapsas

---
+++ HowStringsWork/Program.cs
@@ -0,0 +1,26 @@
+﻿
+string text = "Nick ";
+
+string trimmed = text.TrimEnd(); // Created a new string
+
+var nick1 = "Nick";
+var nick2 = "Nick";
+var result = nick1 == nick2; // True only one is created in the heap because they are equal interns in compiler
+
+{
+    var nick = "Nick";
+    var chapsas = "Chapsas";
+    var nickChapsas = nick + chapsas;
+    var result1 = nickChapsas == "NickChapsas"; // True
+    var result2 = ReferenceEquals(nickChapsas, "NickChapsas"); // False
+}
+{
+    var nick = "Nick";
+    var chapsas = "Chapsas";
+    var nickChapsas = string.Intern(nick + chapsas);
+    var result1 = nickChapsas == "NickChapsas"; // True
+    var result2 = ReferenceEquals(nickChapsas, "NickChapsas"); // True
+
+}
+
+Console.WriteLine();
