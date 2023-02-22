48fb800c8afb9187ee1050104811d121813fd3ad
joao.ramos <ramosjp1993@gmail.com>
2023-02-22 21:14:04 +0000

    Nick Chapsas - The List Best Practice that .NET 8 Makes Possible
    https://www.youtube.com/watch?v=xIA3t2Je3uQ&ab_channel=NickChapsas

---
+++ EmptyCollections/UserService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmptyCollections
+{
+    public record User(string FullName);
+
+    public class UserService
+    {
+        public IEnumerable<User> GetUsers()
+        {
+            // Validation
+            if(!validation)
+            {
+                return Enumerable.Empty<User>();
+                return Array.Empty<User>();
+                return ReadOnlyCollection<User>.Empty();
+                return ReadOnlyDictionary<int, User>.Empty;
+            }
+            // Call to a Database
+
+            return null;
+        }
+    }
+}
