e668e91a157c0c189cd0f5acef8131124953c51a
João Ramos <ramosjp1993@gmail.com>
2025-03-12 22:43:01 +0000

    Drafting Learned Lessons from Rest API Course

---
+++ RESTAPIs/Movies.Api/Controllers/MoviesController.cs
@@ -0,0 +1,91 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+using Movies.API.Mapping;
+using Movies.Application.Models;
+using Movies.Application.Repositories;
+using Movies.Contracts.Requests;
+using Movies.Contracts.Responses;
+
+namespace Movies.API.Controllers;
+
+[ApiController]
+//[Route("api")]
+public class MoviesController : ControllerBase
+{
+    private readonly IMovieRepository _movieRepository;
+
+    public MoviesController(IMovieRepository movieRepository)
+        => _movieRepository = movieRepository;
+
+    //[HttpPost("movies")]
+    [HttpPost(ApiEndpoints.Movies.Create)]
+    public async Task<IActionResult> Create([FromBody] CreateMovieRequest request)
+    {
+        //var movie = new Movie
+        //{
+        //    Id = Guid.NewGuid(),
+        //    Title = request.Title,
+        //    YearOfRelease = request.YearOfRelease,
+        //    Genres = request.Genres.ToList()
+        //};
+        var movie = request.MapToMovie();
+        await _movieRepository.CreateAsync(movie);
+        var response = new MovieResponse
+        {
+            Id = movie.Id,
+            Title = movie.Title,
+            Genres = movie.Genres,
+            YearOfRelease = movie.YearOfRelease
+        };
+        return CreatedAtAction(nameof(Get), new { id = movie.Id }, movie);
+        //return Created($"/{ApiEndpoints.Movies.Create}/{movie.Id}", response);
+    }
+
+    [HttpGet(ApiEndpoints.Movies.Get)]
+    public async Task<IActionResult> Get([FromRoute] Guid id)
+    {
+        var movie = await _movieRepository.GetByIdAsync(id);
+        if (movie is null)
+        {
+            return NotFound();
+        }
+
+        var response = movie.MapToResponse();
+        return Ok(response);
+    }
+
+    [HttpGet(ApiEndpoints.Movies.GetAll)]
+    public async Task<IActionResult> GetAll()
+    {
+        var movies = await _movieRepository.GetAllAsync();
+        var moviesResponse = movies.MapToResponse();
+        return Ok(moviesResponse);
+    }
+
+    [HttpPut(ApiEndpoints.Movies.Update)]
+    public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] UpdateMovieRequest request)
+    {
+        var movie = request.MapToMovie(id);
+        var updated = await _movieRepository.UpdateAsync(movie);
+        if(updated is false)
+        {
+            return NotFound();
+        }
+
+        var response = movie.MapToResponse();
+        return Ok(response);
+    }
+
+    [HttpDelete(ApiEndpoints.Movies.Delete)]
+    public async Task<IActionResult> Delete([FromRoute] Guid id)
+    {
+        var deleted = await _movieRepository.DeleteByIdAsync(id);
+        if(deleted is false)
+        {
+            return NotFound();
+        }
+
+        return Ok();
+    }
+}
---
+++ RESTAPIs/Movies.Application/ApplicationServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+using Movies.Application.Repositories;
+
+namespace Movies.Application;
+
+public static class ApplicationServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddSingleton<IMovieRepository, MovieRepository>();
+        return serviceCollection;
+    }
+}
